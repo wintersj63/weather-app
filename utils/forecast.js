70ef7b62fc33157013d64886f34e072f68e64886
Joshua Winters <wintersj63@gmail.com>
2020-08-28 11:15:16 -0500

    Modifying current code to implement shorthand and destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,18 +1,17 @@
 const request = require('request')
 
-
 const forecast =  (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=d708b084721589e833355a187ddde094&query=' + latitude + ', ' + longitude + '&units=f'
 
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         // Add response and error code
         if(error){
             callback(error.code + error.type, undefined)
-        }else if(response.body.error){
+        }else if(body.error){
             callback('Error code: ' + response.body.error.code +  ', ' + response.body.error.info, undefined)
         }else {
             callback(undefined, 
-                response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + '. It feels like ' + response.body.current.feelslike + ' degrees out.', 
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. It feels like ' + body.current.feelslike + ' degrees out.', 
             )
         }
     })
